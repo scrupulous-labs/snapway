d8e4778f0eda4ed8dcd859fb126058d59086534c
Pavan Varma <pavan@scrupulouslabs.com>
2025-10-23 21:18:17 +0530

    Fixes

--- apps/app-server/src/routes/index.tsx
+++ apps/app-server/src/routes/index.tsx
@@ -1,10 +1,10 @@
+import { Editor } from "@snapway/editor";
 import { createFileRoute } from "@tanstack/react-router";
-import TextField from "node_modules/@snapway/editor/src/text-field";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
-  return <TextField />;
+  return <Editor />;
 }
